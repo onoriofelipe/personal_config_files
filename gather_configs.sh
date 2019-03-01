0e64f1228864837253e32fa1fc6e281f639319c4
onoriofelipe_at_fedoratoucan <onoriofelipe7@gmail.com>
2019-03-01 01:34:00 -0300

    Added script to automate installation
    The script assumes the relevant dirs already exist, such as after
    installing conky.

--- gather_configs.sh
+++ gather_configs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# vim config, tmux config, tmuxinator config, bash config
+# vim config, tmux config, tmuxinator config, bash config, conky config
 
 cp ~/.vimrc ~/.tmux.conf ~/.config/tmuxinator/default_tmuxinator.yml ~/.bashrc ~/.conkyrc ~/config_centralization/personal_config_files/ 
 cp /etc/conky/conky.conf ~/config_centralization/personal_config_files/etc/conky/
---
+++ push_configs_to_local_filesystem.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp ./.tmux.conf ~/
+cp ./.vimrc ~/
+cp ./.bashrc ~/
+cp ./.conkyrc ~/
+sudo cp ./etc/conky/conky.conf /etc/conky/
+cp ./.config/autostart/conky.desktop ~/.config/autostart/
+
