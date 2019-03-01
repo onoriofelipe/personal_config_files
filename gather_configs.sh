ebd9f57fa782c0e3fbb5d8bdb420bcd49a3ef976
onoriofelipe <onoriofelipe7@gmail.com>
2019-02-28 23:27:49 -0300

    Started tracking conky default and personalized configurations

--- gather_configs.sh
+++ gather_configs.sh
@@ -2,4 +2,6 @@
 
 # vim config, tmux config, tmuxinator config, bash config
 
-cp ~/.vimrc ~/.tmux.conf ~/.config/tmuxinator/default_tmuxinator.yml ~/.bashrc  ~/config_centralization/personal_config_files/
+cp ~/.vimrc ~/.tmux.conf ~/.config/tmuxinator/default_tmuxinator.yml ~/.bashrc ~/.conkyrc ~/config_centralization/personal_config_files/ 
+cp /etc/conky/conky.conf ~/config_centralization/personal_config_files/etc/conky/
+cp ~/.config/autostart/conky.desktop ~/config_centralization/personal_config_files/.config/autostart/conky.desktop
